326087433425643d63f9a94a17f397a7c0bed11b
cracr <cracroule@gmail.com>
2018-01-04 16:54:56 +0100

    check players have an unique identifier (name attribute)

--- test_validation/test_perudo.py
+++ test_validation/test_perudo.py
@@ -18,13 +18,21 @@ def perudo():
 
 
 def test_roll_all_dices(perudo):
-    my_perudo = perudo
     remaining_dices = [5, 5, 5]
     obtained_dices = perudo.roll_all_dices(remaining_dices)
     for i in range(len(remaining_dices)):
         assert len(obtained_dices[i]) == remaining_dices[i]
 
 
-
 def test_perudo_game(perudo, four_players):
     perudo.play_game(four_players)
+
+
+def test_game_with_same_name_players(perudo, four_players):
+    four_players[0].name = four_players[1].name
+    crashed = False
+    try:
+        perudo.play_game(four_players)
+    except NameError:
+        crashed = True
+    assert crashed
